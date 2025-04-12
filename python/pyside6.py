136447cf3922fa3371ad9fab0511702c7fad67fc
gau <gau@gau-ubuntu24.04>
2025-04-12 11:06:18 +0900

    clean

--- python/pyside6.py
+++
@@ -1,7 +0,0 @@
-from PySide6.QtWidgets import QApplication, QPushButton
-
-app = QApplication([])
-button = QPushButton("Click (PySide6)")
-button.clicked.connect(lambda: print("PySide6 clicked!"))
-button.show()
-app.exec()
